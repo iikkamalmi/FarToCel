72af6c3c54baa1bbcd7843772c2354a348a9a257
iikka <iikka.malmi@hotmail.com>
2024-03-05 19:58:48 +0200

    3 fix

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,8 @@
 FROM maven:latest
-WORKDIR /FarToCel
-COPY pom.xml /FarToCel
-COPY . /FarToCel/
+WORKDIR /App
+COPY pom.xml /App
+COPY . /App/
 RUN mvn package
-CMD ["java", "-jar", "target/FarToCel.jar"]
-ENTRYPOINT ["java", "-jar", "target/FarToCel.jar"]
+CMD ["java", "-jar", "target/interconversions.jar"]
+ENTRYPOINT ["java", "-jar", "target/interconversions.jar"]
 
--- src/main/java/org/example/FarToCel.java
+++ src/main/java/org/example/FarToCel.java
@@ -11,5 +11,11 @@ public class FarToCel {
 
     public static double KelvinToFahrenheit(double kelvin) {
         return ((kelvin - 273.15) * 9 / 5 + 32);
+        // 300.1 kelvin is 80.51F this is wrong -> ((kelvin - 273.15) * (9 / 5) + 32);
+    }
+    public static void main(String[] args) {
+        System.out.println("32 Fahrenheit is " + convertFar(32) + " Celsius");
+        System.out.println("273.15 Kelvin is " + KelvinToCelsius(273.15) + " Celsius");
+        System.out.println("300.1 Kelvin is " + KelvinToFahrenheit(300.1) + " Fahrenheit");
     }
 }
